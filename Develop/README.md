84534e764c2128e82eec4e41adf5bd1e06bfa102
Coffee Pawz <lauralynneduffy@gmail.com>
2022-10-20 15:49:20 -0400

    Added in the node package (not sure if correct) also added in code based on pseudo code

---
+++ Develop/README.md
@@ -0,0 +1,1 @@
+#Title: ${answers.title}
--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of questions for user input Example: const questions = []; 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title', 
+        message: 'Title of  your project:',
+    }, 
+    /*
+    {
+        type: 'input',
+        name: 'motivation', 
+        message: 'What motivated this project?',
+    }, 
 
+    {
+        type: 'input', 
+        name: 'solve',
+        message: 'What problem does this project solve?',
+    },
+    {
+        type: 'input', 
+        name: 'learn',
+        message: 'What did you learn while working on the project?',
+    },
+    {
+        type: 'input',
+        name: 'standout',
+        message: 'What makes your project stand out?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input', 
+        name: 'usage',
+        message: 'Provide instruction and examples for use',
+    },
+    {
+        type: 'input',
+        name: 'collaborators',
+        message: 'List your collaborators, if any',
+    },
+
+    {
+        type: 'input',
+        name: 'liscence',
+        message: 'Liscence type or information on liscence:',
+    },
+    */
+])
+
+.then((answers)=>{       //Create a function to write README file -- Example: fuction writeToFile(fileName, data) {}
+    const content = generateMd(answers); 
+
+    fs.writeFile('README.md', content, (err)=>
+        err ? console.log(err): console.log('Created your README successfully.')
+    );
+});
+
+function generateMd(answers) {
+return (
+   '#Title:' + $(answers.title) //Won't accept dollar sign
+)
+};
+
+
+
+
+//Don't understand the below code: Also the code in utils
 // TODO: Create a function to initialize app
 function init() {}
 
